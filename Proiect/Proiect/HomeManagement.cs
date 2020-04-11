f1725392e63a40ab620f9e380b5de5d17c7465c7
VulpeLoredanaGeorgiana99 <vulpeloredanageorgiana@yahoo.com>
2020-04-11 14:22:00 +0300

    am adaugat fereastra de login si altele , sa vezi cristino

---
+++ Proiect/Proiect/HomeManagement.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proiect
+{
+    public partial class HomeManagement : Form
+    {
+        public HomeManagement()
+        {
+            InitializeComponent();
+        }
+
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+            panelCazare.Controls.Clear();
+
+            tabAddCazare addCazare = new tabAddCazare();
+            addCazare.TopLevel = false;
+            panelCazare.Controls.Add(addCazare);
+            addCazare.FormBorderStyle = FormBorderStyle.None;
+            addCazare.Dock = DockStyle.Fill;
+            addCazare.Show();
+
+        }
+    }
+}
--- Proiect/Proiect/tabAddCazare.cs
+++ Proiect/Proiect/tabAddCazare.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Proiect
 {
-    public partial class Form1 : Form
+    public partial class tabAddCazare : Form
     {
-        public Form1()
+        public tabAddCazare()
         {
             InitializeComponent();
         }
